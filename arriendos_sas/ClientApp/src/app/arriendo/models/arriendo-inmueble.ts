6c3002089db67604abbaa436ad2135bab74a18c7
Orlando Rojano <orlandorojano@hotmail.com>
2020-12-18 19:55:04 -0500

    Intento backend

---
+++ arriendos_sas/ClientApp/src/app/arriendo/models/arriendo-inmueble.ts
@@ -0,0 +1,7 @@
+export class ArriendoInmueble {
+    matriculaInmuebe: string;
+    identificacionArrendatario: string;
+    nombreArrendatario: string;
+    mesesAlquiler: number;
+    valorDeposito: number;
+}
---
+++ arriendos_sas/ClientApp/src/app/arriendo/models/inmueble.ts
@@ -0,0 +1,5 @@
+export class Inmueble {
+    matriculaInmobiliaria: string;
+    direccion: string;
+    valorArriend: number;
+}
