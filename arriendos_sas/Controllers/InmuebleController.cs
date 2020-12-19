bcebde8b49049d8e8fea8321c905ecf7aa824370
Orlando Rojano <orlandorojano@hotmail.com>
2020-12-18 22:31:10 -0500

    Cotrolador Api

---
+++ arriendos_sas/Controllers/InmuebleController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace arriendos_sas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InmuebleController : ControllerBase
+    {
+        // GET: api/<InmuebleController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<InmuebleController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<InmuebleController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<InmuebleController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<InmuebleController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- arriendos_sas/Startup.cs
+++ arriendos_sas/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Datos;
+using Microsoft.EntityFrameworkCore;
 
 namespace arriendos_sas
 {
@@ -20,6 +22,8 @@ namespace arriendos_sas
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ArriendoContext>(p => p.UseSqlServer(connectionString));
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
